242580e25fb91e71fae6998df694fce842b8efa3
MJ Johnson <mjohnson4685@gmail.com>
2020-09-21 01:56:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_reader :discount
   attr_accessor :total
 
-  def initialize(discount = 20)
+  def initialize(discount = 0)
     @total = 0
     @discount = discount
   end
